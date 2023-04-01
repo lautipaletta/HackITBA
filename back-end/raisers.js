4327ff874e16526452fef4ba1143a2d8daad2173
Acuzito55 <asbsola@gmail.com>
2023-04-01 13:36:30 -0300

    added unique username

--- back-end/raisers.js
+++ back-end/raisers.js
@@ -6,17 +6,22 @@ const modelRaiser = require("./models/raiserModel");
 router.get('/get', async function (req, res) {
     const raiser = await modelRaiser.findOne({ id: req.query.id });
     if (raiser) {
-        const {password, ...response} = raiser._doc;
-        res.send({raiser: response});
+        const { password, ...response } = raiser._doc;
+        res.send({ raiser: response });
     } else {
-        res.send({raiser: null});
+        res.send({ raiser: null });
     }
 });
 
 router.post('/new', async function (req, res) {
-    const raiser = new modelRaiser(req.body);
-    await raiser.save();
-    res.send(req.body);
+    const matches = modelRaiser.findOne({id: req.body.id});
+    if (matches) {
+        res.send({match: false});
+    } else {
+        const raiser = new modelRaiser(req.body);
+        await raiser.save();
+        res.send({match: false});
+    }
 });
 
 router.post('/login', async function (req, res) {
@@ -25,11 +30,11 @@ router.post('/login', async function (req, res) {
     const response = await modelRaiser.findOne({ name: name });
     if (response) {
         if (raiser.password == password) {
-            res.send({raiser: response});
+            res.send({ raiser: response });
             return;
         }
     }
-    res.send({raiser: null});
-    });
+    res.send({ raiser: null });
+});
 
 module.exports = router;
