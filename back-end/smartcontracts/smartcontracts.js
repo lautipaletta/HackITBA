ee8e29f9fbadb5a6fc3cd97fdaf71694024b3de9
Ignacio Searles <nachosearles3@gmail.com>
2023-04-01 13:57:48 -0300

    dockerfile

--- back-end/smartcontracts/smartcontracts.js
+++ back-end/smartcontracts/smartcontracts.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
 const fs = require("fs");
 
-const web3 = new Web3('http://localhost:7545');
+const web3 = new Web3('http://localhost:8545');
 
 const contractBytecode = fs.readFileSync("crowdfunding_sol_Crowdfund.bin").toString();
 
