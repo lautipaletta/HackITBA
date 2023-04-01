f1e41e4dbf4c2a64324c2c1c68e9dfc482f758ff
Acuzito55 <asbsola@gmail.com>
2023-04-01 01:43:44 -0300

    pls fix nacho

--- back-end/server.js
+++ back-end/server.js
@@ -1,35 +1,46 @@
 const express = require("express");
 const mongo = require("mongodb");
-const course = require("course");
+let MongoClient = mongo.MongoClient;
+const cors = require("cors");
 const helmet = require("helmet");
 const app = express();
 
-app.use(course());
+app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
-var port = 5500;
-const url = 'http://localhost:'+port;
-const client = new MongoClient(url);
+var port = 3000;
+const uri = 'mongodb://localhost:27017/';
+const client = new MongoClient(uri);
 
 app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+console.log("Running!!!");
 
-const dbRaisers = client.db("Raisers");
-const collRaisers = dbRaisers.collection('documents');
+const dbRaisers = client.db("Foundraisers_Database");
+const collRaisers = dbRaisers.collection("Raisers");
+
+collRaisers.insertOne({
+    id: "1234",
+    name: "Augusto",
+    description: "Un tipo peculiar.",
+    contactInfo: "Lujan",
+    profileImageUrl: "",
+    crowdfunds: ""
+});
 
 app.get('/getRaiser', async function(req, res) {
     console.log('receiving data ...');
-    console.log('body is ',req.body);
-    raiser = await dbRaisers.findOne({id: req.body});
-    response = {
-        id: raiser.id,
-        name: raiser.name,
-        description: raiser.description,
-        contactInfo: raiser.contactInfo,
-        profileImageUrl: raiser.profileImageUrl,
-        crowdfunds: raiser.crowdfunds
+    console.log('body is ', req.body);
+    let raiser = await dbRaisers.findOne({id: req.body});
+    let response = {
+        "id": raiser.id,
+        "name": raiser.name,
+        "description": raiser.description,
+        "contactInfo": raiser.contactInfo,
+        "profileImageUrl": raiser.profileImageUrl,
+        "crowdfunds": raiser.crowdfunds
     };
-    res.send(JSON.parse(response));
+    res.send(response);
 });
 
 app.post('/newRaiser', async function(req, res) {
