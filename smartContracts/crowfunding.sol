8b98cbc43ff4612cbc3a7a74f80dca63daa2dae1
Ignacio Searles <nachosearles3@gmail.com>
2023-03-31 23:08:08 -0300

    smart contracts

---
+++ smartContracts/crowfunding.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Crowdfund {
+    address payable public destination;
+
+    address payable[] donors; 
+
+    uint public targetAmount; 
+
+
+}
