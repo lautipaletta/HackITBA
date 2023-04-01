beeaf294530f5764b037db28677a3668693c3915
Valentin <valentin.garfi@gmail.com>
2023-04-01 05:39:27 -0300

    feat: made CampaignCard

--- app/lib/screens/HomeScreen.dart
+++ app/lib/screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:app/classes/Crowdfund.dart';
 import 'package:app/components/CampaignCard.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CampaignCard(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            CampaignCard(crowdfund: Crowdfund(deadline: 9, description: "La escuela Albert Einstein no tiene la plata suficiente para poder afrotnar el gasto que significa 20 libros de matemática, 30 de lengua y los necesarios para la bilbioteca del aula de los chicos. Es fundamental la lectura en esta etapa de sus vidas", goalAmount: 2900, receiver: "Mauricio Macri", title: "Colecta de 200 juguetes y sillas para colegio de La Rioja"), raiser: "Santiago", imageHolder: "assets/png/cardImage.png",),
+            CampaignCard(crowdfund: Crowdfund(deadline: 9, description: "Amamos a los perritos", goalAmount: 2900, receiver: "Mauricio Macri", title: "Devolvamos la plata al FMI rn"), raiser: "Santiago", imageHolder: "assets/png/cardImage.png"),
+          CampaignCard(crowdfund: Crowdfund(deadline: 9, description: "Amamos a los perritos", goalAmount: 2900, receiver: "Mauricio Macri", title: "Urgente!! Las escuelas de Rio NEgro necesitan calfeacción"), raiser: "Santiago", imageHolder: "assets/png/cardImage.png",),
+          
+          
+          ],
+        ),
+      ),
     );
   }
 }
